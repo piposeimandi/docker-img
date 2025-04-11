eea202c8d3f06bd18ffc81cbf810482d6a3257b5
Seimandi Adrian <bajosega@gmail.com>
2025-04-11 13:01:36 -0300

    agregada imagen para front laravel

---
+++ php8.2_composer/build.sh
@@ -0,0 +1,1 @@
+docker build -t  bajosega/php-8.2-apache-composer:latest .
---
+++ php8.2_composer/push.sh
@@ -0,0 +1,2 @@
+# debe estar logeado con el usuario , en este caso bajosega
+docker push bajosega/php-8.2-apache-composer:latest
