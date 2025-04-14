4ee1dc7705aca0c36375904156e3b848368ddc82
Seimandi Adrian <bajosega@gmail.com>
2025-04-14 13:04:51 -0300

    si agrega la contruccion de php 8.4 como ultima version y queda tambien la version 8.2 por las dudas seguir actualizandola .

--- php_composer/build.sh
+++ php_composer/build.sh
@@ -1,1 +1,2 @@
-docker build -t  bajosega/php-apache-composer:latest .
+docker build -f Dockerfile8.2 -t  bajosega/php-apache-composer:8.2 .
+docker build  -t  bajosega/php-apache-composer:latest .
--- php_composer/push.sh
+++ php_composer/push.sh
@@ -1,2 +1,3 @@
 # debe estar logeado con el usuario , en este caso bajosega
-docker push bajosega/php-apache-composer:latest
+docker push bajosega/php-apache-composer:8.2
+docker push bajosega/php-apache-composer:latest 
