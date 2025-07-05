6a8edb28a75afb7765a75ea2dcb3634bcb7970c4
piposeimandi <bajosega@gmail.com>
2025-07-05 20:13:47 -0300

    Agregamos herramientas como nano y ping a las imágenes existentes y añadimos una nueva imagen con PHP 8.1

--- api_laravel_sqlsrv/push.sh
+++ api_laravel_sqlsrv/push.sh
@@ -1,4 +1,5 @@
 # debe estar logeado con el usuario , en este caso bajosega
 docker push bajosega/api_laravel_sqlsrv:latest
 docker push bajosega/api_laravel_sqlsrv:8.2
+docker push bajosega/api_laravel_sqlsrv:8.1
 read -p "Presiona [Enter] para continuar..."
