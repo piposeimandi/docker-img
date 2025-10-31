8ef0ada23059409333871ea4a90a80440dfd852d
Seimandi Adrian <bajosega@gmail.com>
2025-10-31 14:17:30 -0300

    tratando de crear una imagen para jenkins .

---
+++ jenkins_pipeline/build.sh
@@ -0,0 +1,3 @@
+docker build -t bajosega/jenkins_pipeline_php:8.2 .
+#docker build -f Dockerfile8.2 -t  bajosega/api_laravel_sqlsrv:8.2 .
+read -p "Presiona [Enter] para continuar..."
---
+++ jenkins_pipeline/push.sh
@@ -0,0 +1,3 @@
+# debe estar logeado con el usuario , en este caso bajosega
+docker push bajosega/jenkins_pipeline_php:8.2
+read -p "Presiona [Enter] para continuar..."
