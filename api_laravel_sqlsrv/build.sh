b4d0349664e33ff237db132337864fbbdedf0ff9
Seimandi Adrian <bajosega@gmail.com>
2025-12-17 12:40:42 -0300

    descomentar builds

--- api_laravel_sqlsrv/build.sh
+++ api_laravel_sqlsrv/build.sh
@@ -1,4 +1,4 @@
-#docker build -t bajosega/api_laravel_sqlsrv:latest .
+docker build -t bajosega/api_laravel_sqlsrv:latest .
 docker build -f Dockerfile8.2 -t  bajosega/api_laravel_sqlsrv:8.2 .
-#docker build -f Dockerfile8.1 -t  bajosega/api_laravel_sqlsrv:8.1 .
+docker build -f Dockerfile8.1 -t  bajosega/api_laravel_sqlsrv:8.1 .
 read -p "Presiona [Enter] para continuar..."
